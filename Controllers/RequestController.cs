6d72dbfa252c09ff189f15e4d6ee079985fc08a4
barmao <bryanbarmao@gmail.com>
2022-08-29 12:32:23 +0300

    Added Polly policies

--- Controllers/RequestController.cs
+++ Controllers/RequestController.cs
@@ -1,12 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RequestService.Polices;
 
 namespace RequestService.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class RequestController : ControllerBase
     {
+        private readonly ClientPolicy _clientPolicy;
+
+        public RequestController(ClientPolicy clientPolicy)
+        {
+            _clientPolicy = clientPolicy;
+        }
+
         public async Task<ActionResult> MakeRequest()
         {
-            return Ok();
+            var client = new HttpClient();
+            var response = await _clientPolicy.LinearHttpRetry.ExecuteAsync(()
+                => client.GetAsync("http://localhost:5168/api/response/25"));
+
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("--> ResponseService returned a Success");
+                return Ok();
+            }
+            else
+            {
+                Console.WriteLine("--> ResponseService returned a FAILURE");
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
     }
 }
---
+++ Polices/ClientPolicy.cs
@@ -0,0 +1,27 @@
+﻿using Polly;
+using Polly.Retry;
+
+namespace RequestService.Polices
+{
+    public class ClientPolicy
+    {
+        public AsyncRetryPolicy<HttpResponseMessage> ImmediateHttpRetry { get; }
+        public AsyncRetryPolicy<HttpResponseMessage> LinearHttpRetry { get; }
+        public AsyncRetryPolicy<HttpResponseMessage> ExponentialHttpRetry { get; }
+
+        public ClientPolicy()
+        {
+            ImmediateHttpRetry = Policy.HandleResult<HttpResponseMessage>(
+                res => !res.IsSuccessStatusCode)
+                .RetryAsync(10);
+
+            LinearHttpRetry = Policy.HandleResult<HttpResponseMessage>(
+                res => !res.IsSuccessStatusCode)
+                .WaitAndRetryAsync(5, retryAttempt => TimeSpan.FromSeconds(3));
+
+            ExponentialHttpRetry = Policy.HandleResult<HttpResponseMessage>(
+                res => !res.IsSuccessStatusCode)
+                .WaitAndRetryAsync(5, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+        }
+    }
+}
