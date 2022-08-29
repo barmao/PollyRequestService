9cbbfc66cd8ced24af31d5006b7301edd49914d1
barmao <bryanbarmao@gmail.com>
2022-08-29 11:34:32 +0300

    first commit

---
+++ Controllers/RequestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RequestService.Controllers
+{
+    public class RequestController : ControllerBase
+    {
+        public async Task<ActionResult> MakeRequest()
+        {
+            return Ok();
+        }
+    }
+}
